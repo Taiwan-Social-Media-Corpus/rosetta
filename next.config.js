a889fcb152025b090bbb278fe713d41960d3ae02
Retr0327 <retr0327.dev@gmail.com>
2023-11-25 21:55:59 +0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+};
+module.exports = withBundleAnalyzer(nextConfig);
