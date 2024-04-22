82f556291ba11a0b202d7b8c7c448d32ae9050e9
Retr0327 <retr0327.dev@gmail.com>
2024-04-23 00:40:34 +0800

    refactor: remove navbar opened state handling (ref #1)

--- src/components/Shell/Navbar/index.tsx
+++ src/components/Shell/Navbar/index.tsx
@@ -1,24 +1,21 @@
-import { Box, ScrollArea } from '@mantine/core';
+import { AppShell, ScrollArea } from '@mantine/core';
 import { NAVBAR_LINKS_DATA } from '@config/links';
 import classes from './Navbar.module.css';
 import NavbarLinksGroup from './LinksGroup';
 
 interface NavbarProps {
-  navbarOpened: boolean;
   onNavbarClose: () => void;
 }
 
 function Navbar(props: NavbarProps) {
-  const { navbarOpened, onNavbarClose } = props;
-
   return (
-    <Box component="nav" className={classes.navbar} mod={{ hidden: !navbarOpened }}>
+    <AppShell.Navbar>
       <ScrollArea className={classes.scrollarea} type="never" offsetScrollbars={false}>
         <div className={classes.body}>
-          <NavbarLinksGroup data={NAVBAR_LINKS_DATA} onNavbarClose={onNavbarClose} />
+          <NavbarLinksGroup data={NAVBAR_LINKS_DATA} onNavbarClose={props.onNavbarClose} />
         </div>
       </ScrollArea>
-    </Box>
+    </AppShell.Navbar>
   );
 }
 
