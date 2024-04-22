4188cc387bdac2c298da02b6aba974725e616d84
Retr0327 <retr0327.dev@gmail.com>
2024-04-22 15:19:35 +0800

    feat: define custom variables for UI styling (ref #3)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,26 @@
 module.exports = {
   plugins: {
-    "postcss-preset-mantine": {},
-    "postcss-simple-vars": {
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
       variables: {
-        "mantine-breakpoint-xs": "36em",
-        "mantine-breakpoint-sm": "48em",
-        "mantine-breakpoint-md": "62em",
-        "mantine-breakpoint-lg": "75em",
-        "mantine-breakpoint-xl": "88em",
+        'mantine-breakpoint-xs': '36em',
+        'mantine-breakpoint-sm': '48em',
+        'mantine-breakpoint-md': '62em',
+        'mantine-breakpoint-lg': '75em',
+        'mantine-breakpoint-xl': '88em',
+        'facebook-color': '#1778F2',
+        'facebook-color-hover': '#4390F3',
+        'github-color': '#000000',
+        'github-color-hover': '#575757',
+        'table-content-width': '260px',
+        'header-height': '60px',
+        'navbar-width': '260px',
+        'navbar-breakpoint': '47.5em',
+        'footer-breakpoint-tablet': '62.5em',
+        'footer-breakpoint-mobile': '40em',
+        'footer-width': '260px',
+        'content-breakpoint': '67.5em',
+        'content-width': '820px',
       },
     },
   },
