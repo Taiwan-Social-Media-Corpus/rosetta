3132a191ec19a38c5e4ad934e5ae36f5dfdf093c
Retr0327 <retr0327.dev@gmail.com>
2024-04-24 16:28:54 +0800

    feat: include user controls (ref #1)

--- src/components/Shell/Header/Controls/index.tsx
+++ src/components/Shell/Header/Controls/index.tsx
@@ -1,15 +1,21 @@
 import { BoxProps, Group, Tooltip } from '@mantine/core';
 import { Href } from '@config';
+import { User } from 'types';
 import { ColorSchemeControl, FacebookControl, GithubControl, UserControl } from './Icons';
 
-function HeaderControls(props: BoxProps) {
+interface HeaderControlsProps extends BoxProps {
+  user: User | null;
+}
+
+function HeaderControls(props: HeaderControlsProps) {
+  const { user, ...rest } = props;
   return (
     <Tooltip.Group openDelay={600} closeDelay={100}>
-      <Group gap="xs" {...props}>
+      <Group gap="xs" {...rest}>
         <FacebookControl link={Href.Facebook} />
         <GithubControl link={Href.Github.Organization} />
         <ColorSchemeControl />
-        <UserControl />
+        <UserControl user={user} />
       </Group>
     </Tooltip.Group>
   );
