b6058cdc685c383382c1ef4a242ce59f99d3c13a
Retr0327 <retr0327.dev@gmail.com>
2024-04-25 17:48:34 +0800

    feat: define types for useForm hook (ref #2)

---
+++ src/types/form.ts
@@ -0,0 +1,87 @@
+import {
+  PasswordInputProps as MantinePasswordInputProps,
+  RadioGroupProps as MantineRadioGroupProps,
+  RadioProps,
+  SelectProps as MantineSelectProps,
+  TextareaProps as MantineTextareaProps,
+  TextInputProps as MantineTextInputProps,
+  CheckboxGroupProps as MantineCheckboxGroupProps,
+  CheckboxProps,
+  NumberInputProps as MantineNumberInputProps,
+  MultiSelectProps as MantineMultiSelectProps,
+  FileInputProps as MantineFileInputProps,
+  SwitchGroupProps as MantineSwitchGroupProps,
+  SwitchProps,
+  GridColProps,
+  StackProps,
+} from '@mantine/core';
+import { DateInputProps as MantineDateInputProps } from '@mantine/dates';
+import { ReactNode } from 'react';
+import { FieldValues, UseFormReturn } from 'react-hook-form';
+
+export type Option<OtherProps = {}> = {
+  label: ReactNode;
+  value: any;
+} & Omit<OtherProps, 'value' | 'label'>;
+
+export interface Options<OtherProps = {}> {
+  options: Option<OtherProps>[];
+}
+
+export type Controlled<T> = { label: ReactNode; name: string } & T;
+export type TextInputProps = Controlled<MantineTextInputProps>;
+export type PasswordInputProps = Controlled<MantinePasswordInputProps>;
+export type TextareaProps = Controlled<MantineTextareaProps>;
+export type NumberInputProps = Controlled<MantineNumberInputProps>;
+export type DateInputProps = Controlled<MantineDateInputProps>;
+export type FileInputProps<T extends boolean> = Controlled<MantineFileInputProps<T>>;
+export type SelectProps = Controlled<
+  Omit<MantineSelectProps, 'data'> & {
+    options: MantineSelectProps['data'];
+  }
+>;
+export type MultiSelectProps = Controlled<
+  Omit<MantineMultiSelectProps, 'data'> & {
+    options: MantineMultiSelectProps['data'];
+  }
+>;
+export type CheckboxGroupProps = Controlled<
+  Omit<MantineCheckboxGroupProps, 'children'> & Options<CheckboxProps> & { stackProps?: StackProps }
+>;
+export type RadioGroupProps = Controlled<
+  Omit<MantineRadioGroupProps, 'children'> & Options<RadioProps> & { stackProps?: StackProps }
+>;
+export type SwitchGroupProps = Controlled<
+  Omit<MantineSwitchGroupProps, 'children'> & Options<SwitchProps> & { stackProps?: StackProps }
+>;
+
+export type ControllerProps =
+  | ({ control: 'checkbox-group' } & CheckboxGroupProps)
+  | ({ control: 'date-input' } & DateInputProps)
+  | ({ control: 'file-input' } & FileInputProps<boolean>)
+  | ({ control: 'multi-select' } & MultiSelectProps)
+  | ({ control: 'number-input' } & NumberInputProps)
+  | ({ control: 'password-input' } & PasswordInputProps)
+  | ({ control: 'radio-group' } & RadioGroupProps)
+  | ({ control: 'select' } & SelectProps)
+  | ({ control: 'switch-group' } & SwitchGroupProps)
+  | ({ control: 'text-area' } & TextareaProps)
+  | ({ control: 'text-input' } & TextInputProps);
+
+export type ControllerMap<TFieldValues extends FieldValues, TContext> = {
+  [key in keyof TFieldValues]-?: ControllerProps & { name: key } & {
+    col?: GridColProps;
+    Field?: (props: {
+      ctx: UseFormReturn<TFieldValues, TContext>;
+      fieldComponent: ReactNode;
+    }) => ReactNode;
+  };
+};
+
+export type ControllerMapResolver<TFieldValues extends FieldValues = FieldValues, TContext = any> =
+  | ControllerMap<TFieldValues, TContext>
+  | ((context: UseFormReturn<TFieldValues, TContext>) => ControllerMap<TFieldValues, TContext>);
+
+export type FormControllerProps<TFieldValues extends FieldValues = FieldValues, TContext = any> = {
+  controllers: ControllerMapResolver<TFieldValues, TContext>;
+};
