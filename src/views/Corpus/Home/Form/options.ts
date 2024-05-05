ce64a442d1f0d6b17d19946c6234a0c33145b3d1
Retr0327 <retr0327.dev@gmail.com>
2024-05-06 01:27:31 +0800

    feat: implement dynamic option generation based on selected media (ref #2)

---
+++ src/views/Corpus/Home/Form/options.ts
@@ -0,0 +1,42 @@
+import { Boards } from 'types';
+import type { ComboboxData } from '@mantine/core';
+
+export const mediaOptions: ComboboxData = [
+  { label: 'All', value: 'all' },
+  { label: 'PTT', value: 'ptt' },
+  { label: 'Dcard', value: 'dcard' },
+];
+
+const mediaPostTypes: { [key: string]: ComboboxData } = {
+  ptt: [
+    { label: 'commentPos', value: 'commentPos' },
+    { label: 'commentNeg', value: 'commentNeg' },
+    { label: 'commentNeu', value: 'commentNeu' },
+  ],
+  dcard: [
+    { label: 'commentPos', value: 'commentPos' },
+    { label: 'commentMale', value: 'commentMale' },
+    { label: 'commentFemale', value: 'commentFemale' },
+  ],
+};
+
+export const defaultPostTypeOptions: ComboboxData = [
+  { label: 'All', value: 'all' },
+  { label: 'Title', value: 'title' },
+  { label: 'Body', value: 'body' },
+  { label: 'Comment', value: 'comment' },
+];
+
+export function createPostTypeOptions(media: string) {
+  if (media === 'all') return defaultPostTypeOptions;
+  const additionalOptions = mediaPostTypes[media as keyof typeof mediaPostTypes];
+  return defaultPostTypeOptions.concat(additionalOptions);
+}
+
+export function createBoardsOptions(boards: Boards, media: string) {
+  if (media === 'all') return [];
+  return boards[media].map((value) => ({
+    label: value,
+    value,
+  }));
+}
