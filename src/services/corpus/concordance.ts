54b16597663e8e31adc9548486234b3ef8eaccf9
Retr0327 <retr0327.dev@gmail.com>
2024-05-10 00:43:04 +0800

    refactor: refine concordance retrieval return type (ref #2)

--- src/services/corpus/concordance.ts
+++ src/services/corpus/concordance.ts
@@ -5,13 +5,16 @@ import { serverRequest } from '@utils/request/server';
 import { ResponseData } from 'types/request';
 import { PickAsOrNull } from 'types';
 import { ConcordanceFormValue } from '@views/Corpus/Home/Form/schema';
+import { ConcordanceData } from 'types/corpus';
 
 type Payload = PickAsOrNull<ConcordanceFormValue, 'media' | 'boards' | 'postType'> & {
   page: number;
 };
 
-export async function getConcordance(payload: Payload) {
-  const [data, error] = await serverRequest<Payload, ResponseData<any>>({
+export async function getConcordance(
+  payload: Payload
+): Promise<readonly [ConcordanceData, null] | readonly [null, any]> {
+  const [data, error] = await serverRequest<Payload, ResponseData<ConcordanceData>>({
     url: serverUrl(API.corpus.concordance),
     method: 'POST',
     payload,
